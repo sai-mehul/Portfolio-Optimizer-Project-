6b7fadf71b91c710e2f689eba6a1cadbe54dd59c
sai-mehul <93685288+sai-mehul@users.noreply.github.com>
2026-05-08 22:17:59 +0530

    Refactor optimizer functions for better performance
    Refactor the port_variance function to return a dictionary with best volatility, weights, and best possible returns. Update efficient_frontier function to calculate optimal weights for given targets.(its still a WIP)

--- optimizer.py
+++ optimizer.py
@@ -53,7 +53,8 @@ def port_variance(price_data,sims):
 
     port_volatility = np.sqrt(port_variance)
 
-    best_volatility = 0
+    best_volatility = float('inf')
+    best_return = 0
 
     for i in range(sims):
         nw = np.random.random(len(returns.columns))
@@ -64,11 +65,62 @@ def port_variance(price_data,sims):
         if new_volatility < port_volatility:
             best_volatility = new_volatility
             init_weights = nw
+            best_return = np.dot(nw,mean_returns)
+    return{"volatility": best_volatility,"weights": init_weights,"best possible returns":best_return}
 
-    return f"Best volatility {best_volatility} with optimal weights {init_weights}"
 
+
+
+
+def efficient_frontier(price_data, risk_free_rate, n_points, sims,tolerance=0.0001):
+    returns = price_data.pct_change().dropna()
+    mean_returns = returns.mean()
+    cov_matrix = returns.cov()
+
+    data_for_use = port_variance(price_data,sims)
+
+    volatility = data_for_use["Volatility"]
+    best_possible_return_with_low_vol = data_for_use["best possible returns"]
+
+    max_return_asset = mean_retruns.idxmax()
+    max_retrun_values = mean_retruns.max()
+
+    max_returns_weight = [0]*len(returns.columns)
+    asset_index = list(returns.columns).index(max_returns)
+    max_return_weight[asset_index] = 1.0
+
+    step_size = (max_return_value - best_possible_return_with_low_vol)/(n_points-1)
     
-    
+    targets = []
+    frontier_points = []
+
+    for i in range(n_points):
+        target = min_vol_retrun + i*step_size
+        targets.append(target)
+
+    weights = []
+
+    for i in range(len(returns.columns)):
+        weights.append(1/len(returns.columns))
+
+    for target in targets:
+        best_vol_for_target = np.inf()
+        best_weights_for_target = weights
+
+        for i in range(sims):
+            nw = np.random.random(len(returns.columns))
+            nw = nw/nw.sum()
+
+            n_pr = np.dot(weights,mean_returns)
+            n_pv = np.sqrt(np.transpose(nw)@cov@nw)
+
+            if abs(n_pr - target)<tolerance:
+
+                if n_pv < best_vol_for_target:
+                    best_vol_for_target = n_pv
+                    best_weights_for_target = nw
+
+        frontier_points.append([target,best_vol,best_weights_for_target])
 
 ############3test data 
 
@@ -77,6 +129,5 @@ price_data = pd.DataFrame({
     'GOOGL': [140.0, 141.5, 143.0, 142.0, 144.0],
     'JPM':   [155.0, 153.0, 157.0, 156.5, 158.0]
 }, index=pd.date_range('2024-01-01', periods=5, freq='B'))
-
 
 
