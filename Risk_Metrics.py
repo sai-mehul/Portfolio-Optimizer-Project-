732f0423bb3368f086d5232b2ceadf7909447c5a
Sai Mehul <93685288+sai-mehul@users.noreply.github.com>
2026-05-11 16:09:38 +0530

    Implement Monte Carlo method for VaR calculation
    Added a new function MC_Var to calculate VaR using Monte Carlo simulation.

--- Risk_Metrics.py
+++ Risk_Metrics.py
@@ -21,6 +21,18 @@ def parametric_var(returns,alpha,portfolio_value):
 
     return Var
 
+def MC_Var(returns,alpha,portfolio_value,n_rets):
+    mu = np.mean(returns)
+    sigma = np.std(returns)
+
+    dist = np.random.normal(mu,sigma,n_rets)
+    dist_sorted = np.sort(dist)
+
+    alpha_percentile = int(alpha*len(dist_sorted))
+
+    VaR = portfolio_value*dist_sorted[alpha_percentile]
+
+    return VaR
 
 ###for testing
 rets = np.array([-0.02, 0.015, -0.04, 0.03, -0.01, 0.005, -0.03, 0.01])
